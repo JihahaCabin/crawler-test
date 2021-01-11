39536ae6a0010c3e6b6ad4fce54c3abe6c49d3c0
zhengzhendu <zdzhengzhendu@163.com>
2021-01-11 19:55:01 +0800

    使用httpClient查询页面

---
+++ src/main/java/com/example/firstcrawler/FirstCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstCrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstcrawler/FirstCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
