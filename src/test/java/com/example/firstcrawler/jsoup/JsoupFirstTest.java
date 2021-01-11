dc433e6fcd877833645378574b43f2228d2332da
zhengzhendu <zdzhengzhendu@163.com>
2021-01-11 23:29:15 +0800

    读取本地的html文件，使用jsoup解析字符串

--- src/test/java/com/example/firstcrawler/jsoup/JsoupFirstTest.java
+++ src/test/java/com/example/firstcrawler/jsoup/JsoupFirstTest.java
@@ -1,11 +1,13 @@
 package com.example.firstcrawler.jsoup;
 
 
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
 import java.net.URL;
 
 /**
@@ -24,5 +26,18 @@ public class JsoupFirstTest {
         String title = doc.getElementsByTag("title").first().text();
         System.out.println(title);
 
+    }
+
+    @Test
+    void testString() throws Exception {
+        //使用工具类读取文件，获取字符串
+        String content = FileUtils.readFileToString(new File("D:\\Java爬虫\\first-crawler\\src\\main\\resources\\baidu.html"), "utf8");
+
+        //解析字符串
+        Document doc = Jsoup.parse(content);
+
+        //使用标签选择器
+        String title = doc.getElementsByTag("title").first().text();
+        System.out.println(title);
     }
 }
